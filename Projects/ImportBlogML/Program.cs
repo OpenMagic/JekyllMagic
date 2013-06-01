3d8767f01934ca0dcd5fb1f98f3c698d57edc34b
Tim Murphy <tim@26tp.com>
2013-06-02 09:15:29 +1000

    Shell for ImportBlogML.

---
+++ Projects/ImportBlogML/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ImportBlogML
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                throw new NotImplementedException("todo");
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
