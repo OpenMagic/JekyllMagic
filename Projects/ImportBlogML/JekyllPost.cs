d2ae143144ca93fb53844fdf6edc2750e5363e62
Tim Murphy <tim@26tp.com>
2013-06-03 07:05:47 +1000

    Completed processing BlogML.xml

---
+++ Projects/ImportBlogML/JekyllPost.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Utilities.FileFormats.BlogML;
+
+namespace ImportBlogML
+{
+    public static class JekyllPost
+    {
+        public static void Convert(Post post, string outputFolder)
+        {
+            var postFileName = Path.Combine(outputFolder, GetPostFileName(post));
+
+            Console.WriteLine("Writing {0}...", postFileName);
+
+            var text = new StringBuilder();
+
+            text.AppendLine("---");
+            text.AppendLine("layout: post");
+            text.AppendLine("title: " + post.Title);
+
+            if (post.Tags != null && post.Tags.TagList != null && post.Tags.TagList.Any())
+            {
+                throw new System.Exception("not tested");
+                text.AppendLine("tags:");
+
+                foreach (var tag in post.Tags.TagList)
+                {
+                    text.AppendLine("- " + tag.REF);
+                }                    
+            }
+            text.AppendLine("---");
+            text.AppendLine(post.Content);
+
+            File.WriteAllText(postFileName, text.ToString());
+        }
+
+        private static string GetPostFileName(Post post)
+        {
+            var fileName = post.PostURL.ToLowerInvariant();
+
+            // Remove /post/ from start of file name.
+            fileName = fileName.Substring(6);
+
+            // Remove .aspx file extension.
+            fileName = fileName.Substring(0, fileName.Length - 5);
+
+            fileName = fileName.Replace("/", "-") + ".md";
+
+            return fileName;
+        }
+    }
+}
--- Projects/ImportBlogML/Program.cs
+++ Projects/ImportBlogML/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using Utilities.FileFormats.BlogML;
 
 namespace ImportBlogML
 {
@@ -9,7 +11,30 @@ namespace ImportBlogML
         {
             try
             {
-                throw new NotImplementedException("todo");
+                var rootFolder = Directory.GetCurrentDirectory();
+
+#if DEBUG
+                rootFolder = @"C:\Users\Tim\Code\tim.26tp.com\import";
+#endif
+
+                var importFileName = Path.Combine(rootFolder, "BlogML.xml");
+                var outputFolder = Path.Combine(rootFolder, "_posts");
+
+#if DEBUG
+                if (Directory.Exists(outputFolder))
+                {
+                    Directory.Delete(outputFolder, true);
+                }
+#endif
+
+                Validate(outputFolder, importFileName);
+                
+                var blogML = new BlogML(importFileName);
+
+                foreach (var post in blogML.Posts.PostList)
+                {
+                    JekyllPost.Convert(post, outputFolder);
+                }                
             }
             catch (Exception ex)
             {
@@ -24,6 +49,26 @@ namespace ImportBlogML
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void Validate(string outputFolder, string importFileName)
+        {
+            if (!File.Exists(importFileName))
+            {
+                throw new FileNotFoundException(string.Format("Import file '{0}' not found.", importFileName), importFileName);
+            }
+
+            if (Directory.Exists(outputFolder))
+            {
+                if (Directory.EnumerateFiles(outputFolder).Any())
+                {
+                    throw new InvalidOperationException(string.Format("Output folder '{0}' must be empty.", outputFolder));
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(outputFolder);
+            }
         }
     }
 }
